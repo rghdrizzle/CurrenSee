9059f6c702dce69e3a5f04a6f5fce207e60e8c40
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2023-06-17 12:40:22 +0530

    code: Updated commands

--- cmd/convert.go
+++ cmd/convert.go
@@ -48,13 +48,4 @@ func init() {
 	convertCmd.Flags().StringP("from","f","USD","The currency you want to convert from")
 	convertCmd.Flags().StringP("to","t","INR","The currency you want to convert to")
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// convertCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// convertCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/root.go
+++ cmd/root.go
@@ -10,7 +10,7 @@ import (
 )
 var version = "0.0.1"
 
-// rootCmd represents the base command when called without any subcommands
+
 var rootCmd = &cobra.Command{
 	Use:   "currensee",
 	Version: version,
@@ -22,8 +22,6 @@ var rootCmd = &cobra.Command{
 	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -33,14 +31,7 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.CurrenSee.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+	
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
