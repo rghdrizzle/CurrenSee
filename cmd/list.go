2e6a684b49ab708b329278e1d911557975e2cd40
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2023-06-17 15:52:09 +0530

    code: added a subcommand for list
    Instead of having listCryptos as a separate command , I made it as a subcommand for the list command.

--- cmd/list.go
+++ cmd/list.go
@@ -13,7 +13,7 @@ import (
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "list to view all the currencies",
+	Short: "list to view all the currencies and crypto",
 	Long: `The "list" command is a powerful feature of our CLI tool that displays a comprehensive list of all the current currencies along with their corresponding short forms.This command provides users with a quick reference to easily identify and work with various currencies within the tool.To utilize the "list" command, simply enter "list" in the command line interface.The tool will promptly retrieve and display the currency data, presenting a clean and organized list that includes the currency's short form and its full name.
 Example Usage:
 
@@ -25,6 +25,25 @@ Currencies:
 - GBP: British Pound Sterling
 - JPY: Japanese Yen
 - ...
+
+You can also view all crypto currency by doing the following:
+
+$currensee list crypto
+
+Cryptocurrency List:
+	- BTC: Bitcoin
+	- ETH: Ethereum
+	- XRP: Ripple
+	- LTC: Litecoin
+	- BCH: Bitcoin Cash
+	- ADA: Cardano
+	- XLM: Stellar
+	- DOT: Polkadot
+	- UNI: Uniswap
+	- DOGE: Dogecoin
+	
+	Note:
+	The provided example showcases a few common cryptocurrencies, but the crypto command will display a comprehensive list of various
 `,
 	
 	 Run: func(cmd *cobra.Command, args []string) {
@@ -39,5 +58,6 @@ Currencies:
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+	listCmd.AddCommand(listCryptoCmd)
 
 }
--- cmd/listCrypto.go
+++ cmd/listCrypto.go
@@ -13,17 +13,17 @@ import (
 
 // listCryptoCmd represents the listCrypto command
 var listCryptoCmd = &cobra.Command{
-	Use:   "listCrypto",
+	Use:   "crypto",
 	Short: "View a comprehensive list of cryptocurrencies,",
 	Long: `The listCrypto command allows you to retrieve a list of cryptocurrencies along with their short forms (symbols) and full names. This command is particularly useful for quickly referencing the common abbreviations used in the cryptocurrency domain.
 
 	Usage:
 	
-	currensee listCrypto
+	currensee crypto
 	
 	Example:
 	
-	$currensee listCrypto
+	$currensee crypto
 	
 	Cryptocurrency List:
 	- BTC: Bitcoin
@@ -40,7 +40,6 @@ var listCryptoCmd = &cobra.Command{
 	Note:
 	The provided example showcases a few common cryptocurrencies, but the listCrypto command will display a comprehensive list of various cryptocurrencies and their corresponding short forms and full names.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("listCrypto called")
 		fmt.Println("Crypto List")
 		fmt.Println("-------------------")
 		currensee.Getcrypto()
@@ -48,6 +47,5 @@ var listCryptoCmd = &cobra.Command{
 }
 
 func init() {
-	rootCmd.AddCommand(listCryptoCmd)
 
 }
